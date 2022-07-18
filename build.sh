d1da4528db9569edf0fe5f6b17c3a86e8e3666ce
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-07-18 18:29:01 +0100

    Better build script

--- build.sh
+++ build.sh
@@ -3,23 +3,43 @@
 # Exit if any errors occur
 set -e
 
-# Options
+# Docker options
 NAME='builder'
 IMAGE='builder:latest'
 
-# Build the docker image if it does not exist
-if [[ ! docker image inspect $IMAGE ]]; then
-	docker image build --pull --file dockerfile --tag $IMAGE /var/empty 2>&1 | tee builder.log
+if [[ "$#" -ne 1 ]]; then
+	echo "Usage: $0 < docker | applications >" 1>&2
+	exit 1
 fi
 
-# Run a container to build the application
-# TODO: Run GCC instead of Bash
-docker run \
-	--name $NAME \
-	--hostname $NAME \
-	--mount type=bind,source=$PWD,target=/repository \
-	--workdir /repository \
-	--entrypoint bash \
-	--interactive \
-	--tty \
-	$IMAGE
+# Build the docker image
+if [[ "$1" = "docker" ]]; then
+	export DOCKER_BUILDKIT=1
+
+	docker image build \
+		--pull \
+		--cache-from $IMAGE \
+		--file dockerfile \
+		--tag $IMAGE \
+		/var/empty \
+		2>&1 | tee docker.log
+
+# Run a container to build the applications
+elif [[ "$1" = "applications" ]]; then
+
+	# TODO: Run GCC instead of Bash
+	docker run \
+		--name $NAME \
+		--hostname $NAME \
+		--mount type=bind,source=$PWD,target=/repository \
+		--workdir /repository \
+		--entrypoint bash \
+		--interactive \
+		--tty \
+		--rm \
+		$IMAGE
+
+else
+	echo "Usage: $0 < docker | applications >" 1>&2
+	exit 1
+fi
