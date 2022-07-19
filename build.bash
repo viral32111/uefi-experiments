3a92cf196ca11a9056b3744b26dbac99085f7612
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-07-19 11:44:43 +0100

    Better build script

--- build.bash
+++ build.bash
@@ -6,25 +6,46 @@ set -e
 # Docker options
 NAME='builder'
 
+# Do not continue unless we have all the arguments
 if [[ "$#" -ne 1 ]]; then
-	echo "Usage: $0 < docker | applications >" 1>&2
+	echo "Usage: $0 < docker [ intermediary | final ] | applications >" 1>&2
+	exit 1
+fi
+
+# Easy access to arguments
+ACTION="$1"
+IMAGE="$2"
+
+# Do not continue if action is empty
+if [[ -z "$ACTION" ]]; then
+	echo "Usage: $0 < docker [ intermediary | final ] | applications >" 1>&2
 	exit 1
 fi
 
 # Build the docker image
 if [[ "$1" = "docker" ]]; then
 
-	# Create directory for logs
-	mkdir -v -p logs/docker
+	# Create logs directory if it does not exist
+	if [[ ! -d "logs/docker" ]]; then
+		mkdir -v -p logs/docker
+	fi
 
-	# Download the base image
+	# Download the base image in case of any updates
 	docker image pull ubuntu:22.04
 
-	# Build the intermediary image
-	docker image build --file dockerfiles/intermediary --tag "$NAME:intermediary" /var/empty 2>&1 | tee logs/docker/intermediary.log
-	
-	# Build the final image
-	docker image build --file dockerfiles/final --tag "$NAME:final" /var/empty 2>&1 | tee logs/docker/final.log
+	# Build just the intermediary image
+	if [[ "$IMAGE" = "intermediary" ]]; then
+		docker image build --file dockerfiles/intermediary --tag "$NAME:intermediary" /var/empty 2>&1 | tee logs/docker/intermediary.log
+
+	# Build just the final image
+	elif [[ "$IMAGE" = "final" ]]; then
+		docker image build --file dockerfiles/final --tag "$NAME:final" /var/empty 2>&1 | tee logs/docker/final.log
+
+	# Build all images
+	else
+		docker image build --file dockerfiles/intermediary --tag "$NAME:intermediary" /var/empty 2>&1 | tee logs/docker/intermediary.log
+		docker image build --file dockerfiles/final --tag "$NAME:final" /var/empty 2>&1 | tee logs/docker/final.log
+	fi
 
 # Run a container to build the applications
 elif [[ "$1" = "applications" ]]; then
@@ -33,14 +54,15 @@ elif [[ "$1" = "applications" ]]; then
 	docker run \
 		--name $NAME \
 		--hostname $NAME \
-		--mount type=bind,source=$PWD,target=/repository \
-		--workdir /repository \
+		--mount type=bind,source=$PWD/applications,target=/applications \
+		--workdir /applications \
 		--entrypoint bash \
 		--interactive \
 		--tty \
 		--rm \
 		"$NAME:final"
 
+# Unrecognised action
 else
 	echo "Usage: $0 < docker | applications >" 1>&2
 	exit 1
